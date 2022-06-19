8637c05bee7375b1b04a18510fa9c843f595e8b8
Luiz Felipe <luizfelipezimbrao@gmail.com>
2022-06-19 16:05:01 -0300

    /feat

--- dist/src/middlewares/cors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Max-Age', '60');
-    next();
-};
--- dist/src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (error, req, res, next) => {
-    res.sendStatus(500);
-    console.log(error);
-    next();
-};
